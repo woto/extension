8b92bf5c065bd99a3423c86a0abf0223a0117f41
Ruslan Kornev <oganer@gmail.com>
2022-04-23 05:15:41 +0300

    temp

--- src/background.ts
+++ src/background.ts
@@ -4,3 +4,32 @@ function polling() {
 }
 
 polling();
+
+
+function onClickHandler(info: any, tab: any) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    if (tab.id) {
+      chrome.tabs.sendMessage(
+        tab.id,
+        {
+          selection: info.menuItemId,
+        },
+        (msg) => {
+          console.log("result message:", msg);
+        }
+      );
+    }
+  });
+
+}
+
+chrome.contextMenus.create({"id": 'selection', "title": 'selection', "contexts":['selection']});
+chrome.contextMenus.create({"id": 'link', "title": 'link', "contexts":['link']});
+
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,20 @@
+let rightclickedItem: EventTarget | null;
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+  console.log(msg);
+  console.log(rightclickedItem);
+
+  // if (msg.color) {
+  //   console.log("Receive color = " + msg.color);
+  //   document.body.style.backgroundColor = msg.color;
+  //   sendResponse("Change color to " + msg.color);
+  // } else {
+  //   sendResponse("Color message is none.");
+  // }
+  
+});
+
+document.body.addEventListener("contextmenu", function(e) {
+  rightclickedItem = e.target;
+  // console.log("context menu in content script handler", rightclickedItem);
 });
