4f9574aacb11f24ebb0edfe2afbb3f1bf8885272
Ruslan Kornev <oganer@gmail.com>
2022-06-11 11:00:17 +0300

    feat: add react-query/devtools

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -11,6 +11,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient()
 
@@ -35,6 +36,7 @@ class AddPopupComponent extends HTMLElement {
     ReactDOM.render(
       <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
           <AppAdd />
         </QueryClientProvider>
       </React.StrictMode>,
