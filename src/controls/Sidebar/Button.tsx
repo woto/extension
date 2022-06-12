67b9907688365effeef7a02d9f51df9bc60b7123
Ruslan Kornev <oganer@gmail.com>
2022-06-12 10:18:09 +0300

    feat: style sidebar button

--- src/controls/Sidebar/Button.tsx
+++ src/controls/Sidebar/Button.tsx
@@ -22,8 +22,8 @@ export default function SlideButton(props: {
     <button
       onClick={(e) => { e.preventDefault(); props.setCurrentTab(props.iconName); }}
       className={`
-            ${props.currentTab === props.iconName ? 'bg-gradient-to-br from-white/0 to-slate-900/20 opacity-80' : ''}
-            fill-slate-800/80 border-r-transparent first:rounded-tr border-r-2 hover:border-r-indigo-400 inline-flex items-center px-2.5 py-3`}
+            ${props.currentTab === props.iconName ? 'bg-gradient-to-bl from-white/0 to-slate-900/20 opacity-80' : ''}
+            fill-slate-600 border-r-transparent first:rounded-tr border-r-2 hover:border-r-indigo-400 inline-flex items-center px-2.5 py-3`}
     >
       <div className="h-5 w-5" dangerouslySetInnerHTML={{ __html: icon }} />
     </button>
