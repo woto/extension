0ca75597b0879ed171a4f0dd0a4f24baa962f3b8
Ruslan Kornev <oganer@gmail.com>
2022-06-17 17:14:07 +0300

    feat: add autofocus to auth popup

--- src/AppAuth.tsx
+++ src/AppAuth.tsx
@@ -75,6 +75,7 @@ function AppAuth() {
                 type="email"
                 autoComplete="email"
                 required
+                autoFocus
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="E-mail адрес"
                 onChange={handleEmailChange}
