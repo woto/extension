51c3aec99d96988ea7d12e682469660eda447084
Ruslan Kornev <oganer@gmail.com>
2022-11-29 02:19:28 +0600

    style: eslint

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,8 +1,8 @@
 // const someCommonValues = ['common', 'values'];
 
-import React from "react";
-import { v4 as uuidv4 } from "uuid";
-import { Entity, GlobalContextInterface } from "../main";
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Entity, GlobalContextInterface } from '../main';
 
 // export const doSomethingWithInput = (theInput) => {
 //    //Do something with the input
@@ -13,12 +13,12 @@ import { Entity, GlobalContextInterface } from "../main";
 //    alert('hello');
 // };
 
-export const appUrl = "https://roastme.ru";
-export const loginUrl = "https://roastme.ru/auth/login";
+export const appUrl = 'https://roastme.ru';
+export const loginUrl = 'https://roastme.ru/auth/login';
 // export const imgproxyUrl = 'http://localhost:8080';
 
 export const GlobalContext = React.createContext<GlobalContextInterface>({
-  apiKey: "",
+  apiKey: '',
   setApiKey: () => {},
   showWindow: true,
   setShowWindow: () => {},
@@ -30,7 +30,7 @@ export const GlobalContext = React.createContext<GlobalContextInterface>({
 export const newLookup = () => ({
   index: uuidv4(),
   id: null,
-  title: "",
+  title: '',
   destroy: false,
 });
 export const newImage = (params: {
@@ -54,29 +54,29 @@ export const newKind = (searchString: string) => ({
   count: null,
 });
 export const newEntity = (searchString: string, entityId?: number): Entity => ({
-  entity_id: entityId?.toString() ?? "",
+  entity_id: entityId?.toString() ?? '',
   title: searchString,
-  intro: "",
+  intro: '',
   images: [],
   lookups: [],
   kinds: [],
   entities_mentions_count: 0,
 });
-export const formatter = Intl.NumberFormat("ru", { notation: "compact" });
+export const formatter = Intl.NumberFormat('ru', { notation: 'compact' });
 
 export enum EntityActionType {
-  INIT = "INIT",
-  SET_TITLE = "SET_TITLE",
-  SET_INTRO = "SET_INTRO",
-  SET_KINDS = "SET_KINDS",
-  APPEND_LOOKUP = "APPEND_LOOKUP",
-  REMOVE_LOOKUP = "REMOVE_LOOKUP",
-  SET_LOOKUP_TITLE = "SET_LOOKUP_TITLE",
-  SET_IMAGES = "SET_IMAGES",
-  APPEND_IMAGE = "APPEND_IMAGE",
-  REMOVE_IMAGE = "REMOVE_IMAGE",
-  REPLACE_IMAGE = "REPLACE_IMAGE",
-  TOGGLE_IMAGE_BACKGROUND = "TOGGLE_IMAGE_BACKGROUND",
+  INIT = 'INIT',
+  SET_TITLE = 'SET_TITLE',
+  SET_INTRO = 'SET_INTRO',
+  SET_KINDS = 'SET_KINDS',
+  APPEND_LOOKUP = 'APPEND_LOOKUP',
+  REMOVE_LOOKUP = 'REMOVE_LOOKUP',
+  SET_LOOKUP_TITLE = 'SET_LOOKUP_TITLE',
+  SET_IMAGES = 'SET_IMAGES',
+  APPEND_IMAGE = 'APPEND_IMAGE',
+  REMOVE_IMAGE = 'REMOVE_IMAGE',
+  REPLACE_IMAGE = 'REPLACE_IMAGE',
+  TOGGLE_IMAGE_BACKGROUND = 'TOGGLE_IMAGE_BACKGROUND',
 }
 
 export const stopPropagation = (e: any) => {
