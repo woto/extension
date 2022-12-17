9fb4d0bf16862b63776665e20016d233d28b7dec
Ruslan Kornev <oganer@gmail.com>
2022-12-18 03:27:00 +0600

    style: eslint

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,18 +1,18 @@
-import "react-devtools";
-import "@webcomponents/webcomponentsjs/webcomponents-bundle.js";
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import 'react-devtools';
+import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import {
   // useQuery,
   // useMutation,
   // useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import tailwind from "./tailwind.css";
-import AppAdd from "./AppAdd";
-import { ToastProvider } from "./ToastManager";
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import tailwind from './tailwind.css';
+import AppAdd from './AppAdd';
+import { ToastProvider } from './ToastManager';
 
 const queryClient = new QueryClient({
   // defaultOptions: {
@@ -26,15 +26,15 @@ class AddPopupComponent extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: "open" });
+    this.attachShadow({ mode: 'open' });
 
-    const mountPoint = document.createElement("div");
-    mountPoint.style.zIndex = "2147483647";
-    mountPoint.style.position = "fixed";
-    mountPoint.style.left = "0";
-    mountPoint.style.top = "0";
-    mountPoint.style.width = "0";
-    mountPoint.style.height = "0";
+    const mountPoint = document.createElement('div');
+    mountPoint.style.zIndex = '2147483647';
+    mountPoint.style.position = 'fixed';
+    mountPoint.style.left = '0';
+    mountPoint.style.top = '0';
+    mountPoint.style.width = '0';
+    mountPoint.style.height = '0';
     // mountPoint.style.transform = 'translate(-10%, -10%)';
     // mountPoint.className = 'w-[320px]';
 
@@ -54,34 +54,34 @@ class AddPopupComponent extends HTMLElement {
           </ToastProvider>
         </QueryClientProvider>
       </React.StrictMode>,
-      mountPoint
+      mountPoint,
     );
   }
 }
 
 const placement = document.documentElement;
 
-customElements.define("add-popup-component", AddPopupComponent);
+customElements.define('add-popup-component', AddPopupComponent);
 
 // customElements.get('add-popup-component')
 
-const component = document.createElement("add-popup-component");
+const component = document.createElement('add-popup-component');
 
 // document.body.insertAdjacentHTML('afterbegin', '<add-popup-component></add-popup-component>');
 
 const subscription = (
   request: any,
   sender: chrome.runtime.MessageSender,
-  sendResponse: (response?: any) => void
+  sendResponse: (response?: any) => void,
 ) => {
   const { message, feature } = request;
 
-  if (feature === "add" && message === "ping") {
+  if (feature === 'add' && message === 'ping') {
     if (!placement.contains(component)) {
-      placement.insertAdjacentElement("afterbegin", component);
+      placement.insertAdjacentElement('afterbegin', component);
     }
 
-    return sendResponse("pong");
+    return sendResponse('pong');
     // let result = ReactDOM.unmountComponentAtNode(mountPoint);
     // console.log('result:', result);
     // let element = document.querySelector('add-popup-component');
@@ -89,6 +89,6 @@ const subscription = (
   }
 };
 
-placement.insertAdjacentElement("afterbegin", component);
+placement.insertAdjacentElement('afterbegin', component);
 
 chrome.runtime.onMessage.addListener(subscription);
